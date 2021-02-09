eb25b1099cab274d00e03a46c77d7f277a3a21bb
Vonlet <31365674+Vonlet@users.noreply.github.com>
2021-02-09 23:01:46 +0900

    Revert "ffff"
    This reverts commit 98efb66a3fe876cea37687a375de8b344e189c3a.

--- src/main/java/com/github/chaosindustry/dimension_conflict/Dimension_conflict.java
+++ src/main/java/com/github/chaosindustry/dimension_conflict/Dimension_conflict.java
@@ -1,27 +1,17 @@
 package com.github.chaosindustry.dimension_conflict;
 
-import com.github.chaosindustry.dimension_conflict.action.PurgePlayer;
-import com.github.chaosindustry.dimension_conflict.command.MotionCommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Dimension_conflict extends JavaPlugin {
 
-    private static Dimension_conflict plugin;
-
     @Override
     public void onEnable() {
-        plugin = this;
-        getServer().getPluginManager().registerEvents(new PurgePlayer(), this);
-        getCommand("DC").setExecutor(new MotionCommandExecutor(this));
-        getLogger().info("モーションプラグインが有効になりました");
+        // Plugin startup logic
+
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("モーションプラグインが無効になりました");
-    }
-
-    public static Dimension_conflict getPlugin(){
-        return plugin;
+        // Plugin shutdown logic
     }
 }
